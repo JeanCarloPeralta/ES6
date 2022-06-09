07b805cb5ae10aea275f549e2f40c970d1ca5663
Jean Carlo Peralta <58532380+JeanCarloPeralta@users.noreply.github.com>
2022-06-09 17:19:05 -0600

    Add files via upload

---
+++ Tarea#4/script.js
@@ -0,0 +1,30 @@
+//.startsWith()
+
+/*
+let string = 'hello world!!!'; 
+console.log(string.startsWith('hello'));
+*/
+
+
+//.endsWith()
+
+/*
+let company = 'TutorialsPoint'
+console.log(company.endsWith('Point'))
+*/
+
+
+//.split()
+
+/*
+let string = "The strongest muscle in the body is the tongue"; 
+let splitted = string.split(" ", 6); 
+console.log(splitted)  
+*/
+
+//.toLowerCase()
+
+/*
+let string = "APPLES ARE ROUND, and Apples are Juicy."; 
+console.log(string.toLowerCase( ))
+*/
